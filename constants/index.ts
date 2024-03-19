f69f47de42d7e5fbbf35e9d6b8859bcd97d9575b
Fagundes <138527223+FagundesDev@users.noreply.github.com>
2024-03-19 12:56:35 -0300

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,364 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/correa10k/",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://www.facebook.com/RealCorreaF/",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://twitter.com",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    image: "mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React Query",
+    image: "reactquery.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "PostgreSQL",
+    image: "postgresql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Prisma",
+    image: "prisma.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Graphql",
+    image: "graphql.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Tauri",
+    image: "tauri.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+  {
+    skill_name: "Figma",
+    image: "figma.png",
+    width: 50,
+    height: 50,
+  },
+] as const;
+
+export const OTHER_SKILL = [
+  {
+    skill_name: "Go",
+    image: "go.png",
+    width: 60,
+    height: 60,
+  },
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "Portfólio Next.js 14 moderno",
+    description:
+      'Embarque em uma jornada pela minha evolução profissional com o "Modern Next.js Portfolio" - uma vitrine dinâmica de minhas habilidades, experiências e paixão pelo desenvolvimento web. Criado com precisão e desenvolvido com Next.js, esse portfólio é mais do que apenas uma exibição estática; é uma experiência imersiva que reflete o que há de mais moderno em tecnologias web.',
+    image: "/projects/project-1.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Portfólio de Cartões Interativos",
+    description:
+      'Entre no mundo extraordinário da minha jornada profissional através do “Portfólio de Cartões Interativos” – uma plataforma inovadora e visualmente cativante que redefine a experiência tradicional do portfólio. Abandonando o layout estático convencional, este portfólio utiliza cartões interativos para mostrar minhas habilidades, projetos e personalidade de maneira envolvente e dinâmica.',
+    image: "/projects/project-2.png",
+    link: "https://example.com",
+  },
+  {
+    title: "Site com tema espacial",
+    description:
+      'Embarque em uma viagem interestelar com meu "Site com tema espacial", um site hipnotizante com tema espacial que convida você a explorar as maravilhas cósmicas além do nosso mundo. Mergulhe em uma experiência digital inspiradora que combina design de ponta com os mistérios do universo.',
+    image: "/projects/project-3.png",
+    link: "https://example.com",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  {
+    title: "Comunidade",
+    data: [
+      {
+        name: "YouTube",
+        icon: FaYoutube,
+        link: "https://youtube.com",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/FagundesDev",
+      },
+      {
+        name: "Discord",
+        icon: RxDiscordLogo,
+        link: "https://discord.gg/EeghQkKdUk",
+      },
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/correa10k/",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://twitter.com/FagundesDev",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/correatc/",
+      },
+    ],
+  },
+  {
+    title: "Sobre",
+    data: [
+      {
+        name: "Torne-se patrocinador",
+        icon: null,
+        link: "https://youtube.com",
+      },
+      {
+        name: "Aprendendo sobre mim",
+        icon: null,
+        link: "https://example.com",
+      },
+      {
+        name: "Fale Comigo",
+        icon: null,
+        link: "RealCorreaF@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "Sobre mim",
+    link: "#about-me",
+  },
+  {
+    title: "Habilidades",
+    link: "#skills",
+  },
+  {
+    title: "Projetos",
+    link: "#projects",
+  },
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://github.com/FagundesDev",
+};
